a34a93628c6741582ff8537294f7f4b7155090f8
Sergio Cardoso <sergioifbaiano@hotmail.com>
2021-01-20 20:07:43 -0300

    Add database

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -1,17 +1,16 @@
 package model.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
 	
+	private DepartmentDao dao = DaoFactory.createDepartmentDao();
+	
 	public List<Department> findAll (){
-		List<Department> list = new ArrayList<>();
-		list.add(new Department (1, "LIVROS"));
-		list.add(new Department (2, "COMPUTADORES"));
-		list.add(new Department (3, "ELETRONICOS"));
-		return list;
+		return dao.findAll();
 	}
 }
